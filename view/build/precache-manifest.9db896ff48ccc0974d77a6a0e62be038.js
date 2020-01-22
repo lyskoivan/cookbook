4d65b0dd99fecee157ebcdb54b2dace66f375402
lyskoivan <lysko1999i@gmail.com>
2020-01-22 23:13:41 +0200

    heroku add build

---
+++ view/build/precache-manifest.9db896ff48ccc0974d77a6a0e62be038.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01f73211a000dce545def5f017f1d038",
+    "url": "/index.html"
+  },
+  {
+    "revision": "005e86f30d28a8debdfa",
+    "url": "/static/css/2.dbaacbbb.chunk.css"
+  },
+  {
+    "revision": "db80a63252dde78f6912",
+    "url": "/static/css/main.2ce8a008.chunk.css"
+  },
+  {
+    "revision": "005e86f30d28a8debdfa",
+    "url": "/static/js/2.189a8050.chunk.js"
+  },
+  {
+    "revision": "bcfcfd76ffa5e8bd579b4e2a4f75269f",
+    "url": "/static/js/2.189a8050.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db80a63252dde78f6912",
+    "url": "/static/js/main.3ce576d4.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
